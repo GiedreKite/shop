79954eb4248b5c888eb96b6ceabc70fb9dc569e8
gintonas <61160755+gintonas@users.noreply.github.com>
2024-08-05 12:36:14 +0300

    fix

--- js/components/PageShopList.js
+++ js/components/PageShopList.js
@@ -1,57 +1,123 @@
 export class PageShopList {
     constructor(DOM) {
         this.DOM = DOM;
+        this.localStorageKey = 'itemList';
+
+        console.log(this.readLocalStorage());
 
         this.render();
         this.listEvents();
     }
 
+    readLocalStorage() {
+        const localStorageData = localStorage.getItem(this.localStorageKey);
+
+        // beda, nes net nera tokio "key" local storidze
+        if (localStorageData === null) {
+            return [];
+        }
+
+        // kazkoks stringas buvo rastas ir bandom ji atstatyti i normalu JS "objekta"
+        // minimali galima teisinga stringo versija yra: []
+        if (typeof localStorageData !== 'string' || localStorageData.length < 2) {
+            return [];
+        }
+
+        const data = JSON.parse(localStorageData);
+
+        if (!Array.isArray(data)) {
+            return [];
+        }
+
+        const validData = [];
+
+        for (const item of data) {
+            if (typeof item === 'object'
+                && item !== null
+                && !Array.isArray(item)
+                && Object.keys(item).length === 3
+                && typeof item.id === 'string'
+                && item.id.length > 5
+                && item.id.startsWith('item_')
+                && isFinite(parseInt(item.id.slice(5)))
+                && typeof item.title === 'string'
+                && item.title.trim().length > 0
+                && typeof item.amount === 'number'
+                && isFinite(item.amount)
+                && item.amount >= 0
+            ) {
+                validData.push(item);
+            }
+        }
+
+        return validData;
+    }
+
+    minus(rowDOM, buttonDOM) {
+        console.log(this);
+
+        const amountChange = +buttonDOM.dataset.step;
+        const idToDecrease = rowDOM.id;
+        const amountDOM = rowDOM.querySelector('span');
+        const list = this.readLocalStorage()
+            .map(item => item.id === idToDecrease
+                ? {
+                    ...item,
+                    amount: item.amount - amountChange > 0 ? (item.amount - amountChange) : 0,
+                }
+                : item);
+
+        localStorage.setItem(this.localStorageKey, JSON.stringify(list));
+        amountDOM.textContent = list.filter(item => item.id === idToDecrease)[0].amount;
+    }
+
+    plus(rowDOM, buttonDOM) {
+        const amountChange = +buttonDOM.dataset.step;
+        const idToIncrement = rowDOM.id;
+        const amountDOM = rowDOM.querySelector('span');
+        const list = this.readLocalStorage()
+            .map(item => item.id === idToIncrement
+                ? {
+                    ...item,
+                    amount: item.amount + amountChange,
+                }
+                : item);
+
+        localStorage.setItem(this.localStorageKey, JSON.stringify(list));
+        amountDOM.textContent = list.filter(item => item.id === idToIncrement)[0].amount;
+    }
+
+    delete(rowDOM, buttonDOM) {
+        const idToRemove = rowDOM.id;
+        const list = this.readLocalStorage().filter(item => item.id !== idToRemove);
+        localStorage.setItem(this.localStorageKey, JSON.stringify(list));
+        rowDOM.remove();
+    }
+
     listEvents() {
         const rowsDOM = this.DOM.querySelectorAll('tbody > tr');
+        const funcList = {
+            minus: this.minus.bind(this),
+            plus: this.plus.bind(this),
+            delete: this.delete.bind(this),
+        };
 
         for (const rowDOM of rowsDOM) {
             const buttonsDOM = rowDOM.querySelectorAll('button');
-            const amountDOM = rowDOM.querySelector('span');
 
-            buttonsDOM[0].addEventListener('click', () => {
-                const idToDecrement = rowDOM.id;
-                const localStorageData = localStorage.getItem('itemList');
-                let list = JSON.parse(localStorageData)
-                    .map(item => item.id === idToDecrement ? { ...item, amount: (item.amount>=1 ? item.amount - 1 : 0)} : item);
-
-                localStorage.setItem('itemList', JSON.stringify(list));
-
-                amountDOM.textContent = list.filter(item => item.id === idToDecrement)[0].amount;
-                if (amountDOM.textContent === 0) {
-                    const idToRemove = rowDOM.id;
-                    const localStorageData = localStorage.getItem('itemList');
-                    const list = JSON.parse(localStorageData).filter(item => item.id !== idToRemove);
-                    localStorage.setItem('itemList', JSON.stringify(list));
-                    rowDOM.remove();
-
-                }
-            });
-
-            buttonsDOM[1].addEventListener('click', () => {
-                const idToIncrement = rowDOM.id;
-                const localStorageData = localStorage.getItem('itemList');
-                const list = JSON.parse(localStorageData)
-                    .map(item => item.id === idToIncrement ? { ...item, amount: item.amount + 1 } : item);
-
-                localStorage.setItem('itemList', JSON.stringify(list));
-
-                amountDOM.textContent = list.filter(item => item.id === idToIncrement)[0].amount;
-            });
-
-            buttonsDOM[2].addEventListener('click', () => {
-                const idToRemove = rowDOM.id;
-                const localStorageData = localStorage.getItem('itemList');
-                const list = JSON.parse(localStorageData).filter(item => item.id !== idToRemove);
-                localStorage.setItem('itemList', JSON.stringify(list));
-                rowDOM.remove();
-            });
+            for (const buttonDOM of buttonsDOM) {
+                buttonDOM.addEventListener('click', () => funcList[buttonDOM.dataset.method](rowDOM, buttonDOM));
+            }
         }
     }
+/*
+            buttonsDOM[0].addEventListener('click', () => this.minus(rowDOM,amountDOM));
+            buttonsDOM[1].addEventListener('click', () => this.plus(rowDOM, amountDOM));
+            buttonsDOM[2].addEventListener('click', () => this.delete(rowDOM));
+*/               
+
+            
+        
 
     render() {
         const data = JSON.parse(localStorage.getItem('itemList'));
@@ -60,18 +126,20 @@ export class PageShopList {
         if (data) {
             for (const item of data) {
                 HTML += `
-                    <tr id="${item.id}">
-                        <td>${item.title}</td>
-                        <td>
-                            <button>-</button>
-                            <span>${item.amount}</span>
-                            <button>+</button>
-                        </td>
-                        <td>
-                            <button>Delete</button>
-                        </td>
-                    </tr>`;
-            }
+                <tr id="${item.id}">
+                    <td>${item.title}</td>
+                    <td>
+                        <button data-method="minus" data-step="2">-2</button>
+                        <button data-method="minus" data-step="1">-1</button>
+                        <span>${item.amount}</span>
+                        <button data-method="plus" data-step="1">+1</button>
+                        <button data-method="plus" data-step="2">+2</button>
+                    </td>
+                    <td>
+                        <button data-method="delete">Delete</button>
+                    </td>
+                </tr>`;
+        }
         }
 
         this.DOM.innerHTML = `
